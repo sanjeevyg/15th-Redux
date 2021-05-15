57ae5910c6f0fce453eba7b328e35592bd879344
sanjeevyg <yogi.sjv@gmail.com>
2021-05-15 14:28:25 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,39 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { combineReducers, createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const count = (state=0, action) => {
+  switch(action.type) {
+    default: 
+      return state
+  }
+}
+
+const characters = (state=[], action) => {
+  switch(action.type) {
+    default: 
+      return state
+    case 'GET_CHARACTERS':
+      return action.characters
+  }
+}
+
+const rootReducers = combineReducers({
+  count, 
+  characters
+})
+
+const store = createStore(rootReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+console.log(store.getState())
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
